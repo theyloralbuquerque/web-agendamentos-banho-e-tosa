99d1f9d4482c3d8fe2c216c2c434afefbe2bc72a
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2024-11-07 23:25:53 -0300

    feat: initial commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Title, Paragrafo } from './styled';
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+      <a href="">Oie</a>
+    </Container>
+  );
+}
