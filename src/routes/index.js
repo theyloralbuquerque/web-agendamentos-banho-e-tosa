c4355413c71e30a9cff0c86c65c8e0aadfc1da7f
Theylor Albuquerque <theylor.oliveira@icc.org.br>
2024-11-19 13:03:51 -0300

    fix: initial configs to routes application

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import MyRoute from './MyRoute';
+
+import Tutor from '../pages/Tutor';
+import Tutores from '../pages/Tutores';
+import Fotos from '../pages/Fotos';
+import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} />
-      <MyRoute exact path="/login" component={Login} />
-      <MyRoute exact path="/*" component={Page404} />
+      <MyRoute exact path="/" component={Tutores} isClosed={false} />
+      <MyRoute exact path="/tutor/:id/edit" component={Tutor} isClosed />
+      <MyRoute exact path="/tutor/" component={Tutor} isClosed />
+      <MyRoute exact path="/fotos/" component={Fotos} isClosed />
+      <MyRoute exact path="/login/" component={Login} isClosed={false} />
+      <MyRoute exact path="/register/" component={Register} isClosed={false} />
+      <MyRoute path="*" component={Page404} />
     </Switch>
   );
 }
