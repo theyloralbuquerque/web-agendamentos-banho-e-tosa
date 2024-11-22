eef3dfc7b887ac970da2a99fdd49d385bfd5200f
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2024-11-21 23:15:57 -0300

    feat: login part-1

--- src/store/modules/example/sagas.js
+++ src/store/modules/example/sagas.js
@@ -2,7 +2,6 @@ import { call, put, all, takeLatest } from 'redux-saga/effects';
 import * as actions from './actions';
 import * as types from '../types';
 import { toast } from 'react-toastify';
-import { GiAlliedStar } from 'react-icons/gi';
 
 const requisicao = () =>
   new Promise((resolve, reject) => {
